736d555f1b8c210a2713409cff6637d8dccef088
JoMinjun9288 <minjun.jo@meshkorea.net>
2022-07-15 09:11:45 +0900

    오류 수정: fetch join 정적 쿼리에 where 절 추가

--- src/main/java/com/vroong/msabootcamp/repository/SongRepository.java
+++ src/main/java/com/vroong/msabootcamp/repository/SongRepository.java
@@ -4,9 +4,10 @@ import com.vroong.msabootcamp.domain.Song;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
 
-  @Query(value = "select song from Song song join fetch song.album")
-  Optional<Song> findByIdFetch(Long songId);
+  @Query(value = "select song from Song song join fetch song.album where song.id = :songId")
+  Optional<Song> findByIdFetch(@Param(value = "songId") Long songId);
 }
